65855dd14febc3fe3308bb689d60fcadbf4ff566
nukotani <nukotani@incel.email>
2026-01-30 23:04:08 +0700

    created basic completion for bash

---
+++ asusctl.bash
@@ -0,0 +1,32 @@
+_get_cmds() {
+#    asusctl $@ --help | awk '/^  [a-z]/ {print $1}'
+    asusctl "$@" --help | awk '
+        /^Commands?:/ {printing=1; next}
+        /^[^[:space:]].*:/ {printing=0} 
+        printing && /^[[:space:]]*[[:alnum:]_-]+/ {gsub(/^[ \t]+/, "", $1); print $1}
+    '
+}
+_get_opts() {
+#    asusctl $@ --help | awk '/^  --/ {print $1}'
+    asusctl "$@" --help | awk '
+        /^Options?:/ {printing=1; next}
+        /^[^[:space:]].*:/ {printing=0}
+        printing && /^[[:space:]]*[[:alnum:]_-]+/ {
+            gsub(/^[ \t]+/, "", $1)
+            gsub(/,$/, "", $1)
+            print $1
+        }
+    '
+}
+
+_asusctl() {
+    compopt +o default
+    local args
+    args=("${COMP_WORDS[@]:1:$COMP_CWORD-1}")
+    COMPREPLY=()
+    opts='$(_get_opts ${args[@]})'
+    cmds='$(_get_cmds ${args[@]})'
+    COMPREPLY=( $(compgen -W "${cmds} ${opts}" -- ${cur}) )
+}
+
+complete -F _asusctl asusctl
